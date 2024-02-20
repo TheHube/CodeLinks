ba10f5e3ddc7e466c3b5419cf6608585b6fec631
TheHube <123021407+TheHube@users.noreply.github.com>
2024-02-20 15:51:34 -0600

    Added Comment

--- MetricsProcessor.cs
+++ MetricsProcessor.cs
@@ -1,4 +1,18 @@
-// MetricsProcessor.cs
+/*
+MetricsProcessor.cs
+
+User Story: Scraping and Storing Metrics
+
+Acceptance Criteria:
+
+    1. Given the web application is running,
+        When 90 seconds pass,
+        Then the web application should scrape the specified URL.
+    2. Given the web application is running,
+        When the web application scrapes the specified URL,
+        Then it should store the metrics in the database using Entity Framework Core.
+*/
+
 public class MetricsProcessor
 {
     public void ScrapeAndStoreMetrics()
